1312c87f6f8433e01ee2c02a0615edddac92745c
Igor Vitenko <igor.vitenko@synthesis.im>
2023-08-28 19:22:54 -0400

    update with a description

--- README.md
+++ README.md
@@ -1,1 +1,51 @@
-# ML-techniques-from-scratch
+# ML Techniques From Scratch
+
+## Purpose
+_By developing ML techniques, we aim to translate the theoretical knowledge available on the Internet into a practical tool._
+
+
+## LinearRegression
+`LinearRegression` is a basic implementation of a linear regression model with options for regularization and different evaluation metrics.  
+
+
+### Features:
+- Gradient Descent Training: Uses gradient descent to optimize the model parameters.  
+- Multiple Evaluation Metrics: Supports various evaluation metrics like Mean Absolute Error (MAE), Mean Squared Error (MSE), Root Mean Squared Error (RMSE), Mean Absolute Percentage Error (MAPE), and R-squared.
+- Regularization: Supports `L1 (Lasso)`, `L2 (Ridge)`, and `ElasticNet` regularization techniques.
+- Stochastic Gradient Descent (SGD): Option to use a subset of data for each iteration of gradient descent.
+
+### Parameters:
+- `n_iter` (int): Number of iterations for gradient descent.
+learning_rate (Union[float, Callable]): Learning rate for gradient descent. Can be a fixed value or a function.
+- `metric` (Optional[str]): Metric for evaluating the model.
+- `reg` (Optional[str]): Regularization type.
+- `l1_coef` (float): Coefficient for L1 regularization.
+- `l2_coef` (float): Coefficient for L2 regularization.
+- `sgd_sample` (Union[int, float, None]): Sample size for stochastic gradient descent.
+- `random_state` (int): Random seed for reproducibility.
+
+### Usage:
+Initialization:
+```python
+model = MyLineReg(n_iter=100, learning_rate=0.1, metric='mse', reg='elasticnet', l1_coef=0.1, l2_coef=0.1, sgd_sample=0.1)
+```
+
+Training:
+```python
+model.fit(X_train, y_train, verbose=10)
+```
+
+Predictions:
+```python
+predictions = model.predict(X_test)
+```
+
+Retrieving Coefficients except `w0`:
+```python
+coefficients = model.get_coef()
+```
+
+Retrieving Last Metric Score:
+```python
+best_score = model.get_best_score()
+```
--- linear_regression.py
+++ linear_regression.py
@@ -6,11 +6,27 @@ import numpy as np
 import pandas as pd
 
 
-class MyLineReg:
+class LinearRegression:
 
     def __init__(self, n_iter: int = 100, learning_rate: Union[float, Callable] = 0.1, metric: Optional[str] = None,
                  reg : Optional[str] = None, l1_coef: float = 0., l2_coef: float = 0.,
-                 sgd_sample: Union[int, float, None] = None, random_state: int = 42) -> None:
+                 sgd_sample: Union[int, float, None] = None, random_state: Optional[int] = 1) -> None:
+        """
+        Initialize the LinearRegression class.
+
+        Parameters:
+        - n_iter (int): Number of iterations for gradient descent.
+        - learning_rate (Union[float, Callable]): Learning rate for gradient descent. Can be a fixed value or a function,
+        which changes its value depending on the gradient descent step, e.g. `lambda step: 0.5 * (0.85 ** step)`
+        - metric (Optional[str]): Metric for evaluating the model (choices: 'mae', 'mse', 'rmse', 'mape', 'r2').
+        - reg (Optional[str]): Regularization type (choices: 'l1', 'l2', 'elasticnet').
+        - l1_coef (float): Coefficient for L1 regularization.
+        - l2_coef (float): Coefficient for L2 regularization.
+        - sgd_sample (Union[int, float, None]): Sample size for stochastic gradient descent. Can be an absolute number, 
+        a fraction of the total, or None.
+        - random_state (int): Random seed for reproducibility. If set to None, each iteration will be chosen random 
+        sample for stochastic grafient descent
+        """
         assert metric in {'mae', 'mse', 'rmse', 'mape', 'r2', None}, f"unexpected metric {metric}"
         assert reg in {'l1', 'l2', 'elasticnet', None}, f"unexpected regularization parameter {reg}"
         assert 0 <= l1_coef <= 1. and 0 <= l2_coef <= 1., f"l1_coef and l2_coef have to be between 0. and 1."
@@ -18,21 +34,21 @@ class MyLineReg:
         self.n_iter = n_iter
         self.learning_rate = learning_rate
         self.weights = np.array([])
-        self.best_metric = None
+        self.best_metric = float('inf')
         self.random_state = random_state
         self.sgd_sample = sgd_sample
         if reg == 'l1':
             self.l1_coef = l1_coef
-            self.l2_coef = 0
+            self.l2_coef = 0.
         elif reg == 'l2':
             self.l2_coef = l2_coef
-            self.l1_coef = 0
+            self.l1_coef = 0.
         elif reg == 'elasticnet':
             self.l1_coef = l1_coef
             self.l2_coef = l2_coef
         else:
-            self.l1_coef = 0
-            self.l2_coef = 0
+            self.l1_coef = 0.
+            self.l2_coef = 0.
     
     def __str__(self) -> str:
         return f"MyLineReg class: n_iter={self.n_iter}, learning_rate={self.learning_rate}"
@@ -40,22 +56,42 @@ class MyLineReg:
     def __repr__(self) -> str:
         return self.__str__()
     
-    def _mse(self, y_p: np.array, y: np.array) -> float:
+    def _mse(self, y_p: np.ndarray, y: np.ndarray) -> float:
+        """Return mean sqared error (MSE) with regularization since this metric used as a loss function
+        https://en.wikipedia.org/wiki/Mean_squared_error
+
+        Parameters:
+        - y_p (np.ndarray): _description_
+        - y (np.ndarray): _description_
+
+        Returns:
+        - float: Mean sqared error
+        """
         return ((y_p - y)**2).sum() / len(y) + self.l1_coef * np.abs(self.weights).sum() + self.l2_coef * (self.weights**2).sum()
 
-    def _mae(self, y_p: np.array, y: np.array) -> float:
+    def _mae(self, y_p: np.ndarray, y: np.ndarray) -> float:
         return (y_p - y).abs().sum() / len(y)
 
-    def _rmse(self, y_p: np.array, y: np.array) -> float:
+    def _rmse(self, y_p: np.ndarray, y: np.ndarray) -> float:
         return math.sqrt(self._mse(y_p, y))
 
-    def _mape(self, y_p: np.array, y: np.array) -> float:
+    def _mape(self, y_p: np.ndarray, y: np.ndarray) -> float:
         return ((y - y_p) / y).abs().sum() * 100 / len(y)
 
-    def _r2(self, y_p: np.array, y: np.array) -> float:
+    def _r2(self, y_p: np.ndarray, y: np.ndarray) -> float:
         return 1 - (((y - y_p)**2).sum() / ((y - y.mean())**2).sum())
 
     def _get_metric(self, **kwargs) -> Optional[float]:
+        """
+        Compute the chosen metric for the model.
+
+        Parameters:
+        - y_p (np.ndarray): Predicted values.
+        - y (np.ndarray): True values.
+        
+        Returns:
+        - float: Computed metric value.
+        """
         if not self.metric:
             return
         elif self.metric == 'mae':
@@ -71,13 +107,25 @@ class MyLineReg:
         else:
             raise Exception(f"Unknown metric '{self.metric}'!")
 
-    def fit(self, X: pd.DataFrame, y: pd.Series, verbose: int = 0) -> None:
+    def fit(self, X: Union[pd.DataFrame, np.ndarray], y: Union[pd.Series, np.ndarray], verbose: int = 0) -> None:
+        """
+        Train the linear regression model using gradient descent.
 
-        X = X.values
+        Parameters:
+        - X (Union[pd.DataFrame, np.ndarray]): Feature matrix.
+        - y (Union[pd.Series, np.ndarray]): Target values.
+        - verbose (Union[int, bool]): If int, print progress every 'verbose' steps. 
+        If verbose = int, print progress every ith step. If False (or 0), no output.
+        """
+        if isinstance(X, pd.DataFrame):
+            X = X.values
+        if isinstance(y, pd.Series) or isinstance(y, pd.DataFrame):
+            y = y.values
+
         X = np.insert(X, 0, 1, axis=1)
         self.weights = np.ones(X.shape[1])
-        random.seed(self.random_state)
-        y = y.values
+        if self.random_state:
+            random.seed(self.random_state)
 
         loss = self._mse
         y_hat = (X @ self.weights)
@@ -104,13 +152,13 @@ class MyLineReg:
                 sample_rows_idx = random.sample(range(X.shape[0]), k)
                 X_smpl, y_hat_sampl, y_sampl = X[sample_rows_idx], y_hat[sample_rows_idx], y[sample_rows_idx]
 
-            nabla = (2 / k * (y_hat_sampl - y_sampl) @ X_smpl) + self.l1_coef * np.sign(self.weights)\
+            gradient = (2 / k * (y_hat_sampl - y_sampl) @ X_smpl) + self.l1_coef * np.sign(self.weights)\
                   + self.l2_coef * 2 * self.weights
             if callable(self.learning_rate):
                 lr = self.learning_rate(i)
             else:
                 lr = self.learning_rate
-            self.weights = self.weights - lr * nabla
+            self.weights = self.weights - lr * gradient
             y_hat = (X @ self.weights)
 
             if self.metric:
@@ -121,15 +169,38 @@ class MyLineReg:
                     verbose_string += f" | {self.metric}: {self.best_metric}"
                 print(verbose_string)
 
-    def predict(self, X: pd.DataFrame) -> np.array:
-        X = X.values
+    def predict(self, X: pd.DataFrame) -> np.ndarray:
+        """
+        Predict target values for the given feature matrix.
+
+        Parameters:
+        - X (pd.DataFrame): Feature matrix.
+        
+        Returns:
+        - np.ndarray: Predicted values.
+        """
+
+        if isinstance(X, pd.DataFrame):
+            X = X.values
         X = np.insert(X, 0, 1, axis=1)
         y_hat = (X @ self.weights)
         return y_hat
 
-    def get_coef(self) -> np.array:
+    def get_coef(self) -> np.ndarray:
+        """
+        Retrieve the coefficients of the trained model except of w0
+
+        Returns:
+        - np.ndarray: Model coefficients.
+        """
         return self.weights[1:]
     
-    def get_best_score(self):
+    def get_best_score(self) -> float:
+        """
+        Get the best score achieved during training based on the chosen metric.
+
+        Returns:
+        - float: Metric score calculated on the last step.
+        """
         return self.best_metric
     
